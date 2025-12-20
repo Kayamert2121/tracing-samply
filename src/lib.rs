865cfc0357e0c60d00cabe2432273592b3afbc99
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-12-20 03:18:17 +0100

    feat: multi threaded works

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 use std::{
+    cell::RefCell,
     fs::{File, OpenOptions},
     io::{self, Write},
     path::Path,
@@ -9,11 +10,15 @@ use std::{
 use tracing_core::{Subscriber, span};
 use tracing_subscriber::{Layer, layer::Context, registry::LookupSpan};
 
+thread_local! {
+    static MARKER_FILE: RefCell<Option<File>> = const { RefCell::new(None) };
+}
+
 /// A tracing layer that records span timings to a file.
 ///
 /// See the [crate docs](self) for more information.
 pub struct PerfMarkersLayer {
-    file: File,
+    dir: Box<Path>,
 }
 
 struct SpanData {
@@ -26,30 +31,45 @@ impl PerfMarkersLayer {
     /// `dir` will contain all the marker files.
     /// If `None`, a temporary directory will be created.
     pub fn new(dir: Option<&Path>) -> io::Result<Self> {
-        let path = match dir {
-            Some(path) => path,
-            None => {
-                let dir = std::env::temp_dir().join("tracing-perf-markers");
-                std::fs::create_dir_all(&dir)
-                    .map_err(map_io_err("could not create perf markers tmp dir", &dir))?;
-                let pid = std::process::id();
-                &dir.join(format!("marker-{pid}.txt"))
-            }
+        let dir = match dir {
+            Some(dir) => dir.to_path_buf(),
+            None => std::env::temp_dir().join("tracing-perf-markers"),
+        }
+        .join(std::process::id().to_string());
+        std::fs::create_dir_all(&dir)
+            .map_err(map_io_err("could not create perf markers dir", &dir))?;
+
+        Ok(Self { dir: dir.into_boxed_path() })
+    }
+
+    fn create_marker_file(&self) -> File {
+        match self.try_create_marker_file() {
+            Ok(file) => file,
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    fn try_create_marker_file(&self) -> io::Result<File> {
+        let pid = std::process::id();
+        let fname = match gettid() {
+            Some(tid) => format!("marker-{pid}-{tid}.txt"),
+            None => format!("marker-{pid}.txt"),
         };
+        let path = &*self.dir.join(fname);
         let file = OpenOptions::new()
             .create_new(true)
             .read(true)
             .write(true)
-            .open(&path)
-            .map_err(map_io_err("could not create perf markers file", &path))?;
+            .open(path)
+            .map_err(map_io_err("could not create perf markers file", path))?;
         // mmap the file to notify samply/perf.
         #[cfg(unix)]
         let _ = unsafe {
             memmap2::MmapOptions::new()
                 .map(&file)
-                .map_err(map_io_err("could not mmap perf markers file", &path))?
+                .map_err(map_io_err("could not mmap perf markers file", path))?
         };
-        Ok(Self { file })
+        Ok(file)
     }
 }
 
@@ -69,12 +89,10 @@ where
         let Some(data) = extensions.get::<SpanData>() else { return };
         let end_ns = now_timestamp();
         let line = format!("{} {} {}\n", data.start_ns, end_ns, span.name());
-        let _ = (&self.file).write_all(line.as_bytes());
+        let _ = MARKER_FILE.with_borrow_mut(|file| {
+            file.get_or_insert_with(|| self.create_marker_file()).write_all(line.as_bytes())
+        });
     }
-}
-
-fn map_io_err(s: &str, p: &Path) -> impl FnOnce(io::Error) -> io::Error {
-    move |e| io::Error::new(e.kind(), format!("{s} {p:?}: {e}"))
 }
 
 fn now_timestamp() -> u64 {
@@ -93,4 +111,19 @@ fn now_timestamp() -> u64 {
     {
         0
     }
+}
+
+fn gettid() -> Option<i32> {
+    #[cfg(target_os = "linux")]
+    {
+        Some(unsafe { libc::gettid() })
+    }
+    #[cfg(not(target_os = "linux"))]
+    {
+        None
+    }
+}
+
+fn map_io_err(s: &str, p: &Path) -> impl FnOnce(io::Error) -> io::Error {
+    move |e| io::Error::new(e.kind(), format!("{s} {p:?}: {e}"))
 }
